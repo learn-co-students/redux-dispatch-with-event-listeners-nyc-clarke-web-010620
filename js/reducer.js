95c5ebd718c6ea6c22ebda6c8cce209e9c08831a
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-05-19 18:23:17 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,37 @@
-// add code snippets from README
+let state;
+
+function reducer(state = { count: 0 }, action) {
+    switch (action.type) {
+        case 'INCREASE_COUNT':
+            return { count: state.count + 1 }
+        case 'DECREASE_COUNT':
+            return { count: state.count - 1 }
+        default:
+            return state;
+    }
+}
+
+function dispatch(action) {
+    state = reducer(state, action)
+    render()
+}
+
+function render() {
+    let div = document.getElementById('container')
+    div.innerText = state.count
+}
+
+let button = document.getElementById('button');
+button.addEventListener('click', () => {
+    dispatch({ type: 'INCREASE_COUNT' })
+})
+
+let button1 = document.getElementById('button-dec');
+button1.addEventListener('click', () => {
+    dispatch({ type: 'DECREASE_COUNT' })
+})
+
+
+// Functions to Execute //
+
+dispatch({ type: '@@INIT' })
